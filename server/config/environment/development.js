767443c939b9590f73ee77baf6078f6df3e7cd17
Samuel Salas <samuel.salas.fernandez@gmail.com>
2017-09-13 10:43:17 +0200

    change the DB for production

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -7,10 +7,10 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://hemicUser:telemedicina@ec2-54-144-199-211.compute-1.amazonaws.com:27017/dummyDB'
+    uri: 'mongodb://localhost/dummyDB'
   },
 
   // Seed database on startup
-  seedDB: false
+  seedDB: true
 
 };
