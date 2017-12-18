9de2a8b3135d457b9a12f2f80dbc94d440488084
Samuel Salas <samuel.salas.fernandez@gmail.com>
2017-12-18 14:05:00 +0100

    NewMachine Configuration

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -7,10 +7,11 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://ec2-34-234-70-89.compute-1.amazonaws.com/dummyDB'
+    uri: 'mongodb://hemicUser:telemedicina@ec2-52-91-46-102.compute-1.amazonaws.com/dummyDB'
+    // uri: 'mongodb://ec2-34-234-70-89.compute-1.amazonaws.com/dummyDB'
   },
 
   // Seed database on startup
-  seedDB: true
+  seedDB: false
 
 };
